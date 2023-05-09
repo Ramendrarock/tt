df35c8cbe840a45f7b4d1648331b24736c7920e6
ramendra <ramendrasingh11291@gmail.com>
2023-05-09 13:17:02 +0530

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+<h1>admin dashboard</h1>
+
+
+
+<form method="POST" action="{{ route('admin.logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('admin.logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
